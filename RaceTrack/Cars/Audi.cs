98fb0d48f572d414b658509c7dc13b740ee0b3cd
MrFiggles7 <rdjohnson7478@gmail.com>
2021-03-09 15:34:52 -0600

    Add project files.

---
+++ RaceTrack/Cars/Audi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    class Audi : RaceCar
+    {
+
+        public Audi()
+        {
+            Name = "Audi";
+            TopSpeed = 200;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine starts");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} ends in position {Position}!");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/ITBob.cs
@@ -0,0 +1,22 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    class ITBob : Driver
+    {
+        public ITBob(RaceCar car) : base(car)
+        {
+            Name = "IT Bob";
+            SkillLevel = 3;
+        }
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
